8b2d7ae0a9f5ffd689705f7fc7f82c7a947dce7e
monkeydluthy <140854666+monkeydluthy@users.noreply.github.com>
2023-11-18 10:38:44 -0500

    Updated portfolio page and style - Carousel, JS

---
+++ resources/js/portfolio.js
@@ -0,0 +1,19 @@
+let coffee = document.querySelector('[data-coffee]');
+coffee.addEventListener('click', () => {
+    location.href = "its-coffee/index.html";
+})
+
+let na = document.querySelector('[data-na]');
+na.addEventListener('click', () => {
+    location.href = "http://www.tampa-na.org";
+})
+
+let security = document.querySelector('[data-security]');
+security.addEventListener('click', () => {
+    location.href = "indoor-security/index.html";
+})
+
+let carousel = document.querySelector('[data-carousel]');
+carousel.addEventListener('click', () => {
+    location.href = "carousel-example/index.html";
+})
