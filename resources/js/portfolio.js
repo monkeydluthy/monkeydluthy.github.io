d0a1a150e1109873b9a1d7282976a02b231358a4
monkeydluthy <140854666+monkeydluthy@users.noreply.github.com>
2023-11-23 12:26:12 -0500

    Site updates

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -17,3 +17,23 @@ let carousel = document.querySelector('[data-carousel]');
 carousel.addEventListener('click', () => {
     location.href = "carousel-example/index.html";
 })
+
+let lyfestyle = document.querySelector('[data-clyde]');
+lyfestyle.addEventListener('click', () => {
+    location.href = "lyfestyle-tattoos/index.html";
+})
+
+let dropdown = document.querySelector('[data-dropdown]');
+dropdown.addEventListener('click', () => {
+    location.href = "dropdown-example/index.html";
+})
+
+let shopping = document.querySelector('[data-shopping]');
+shopping.addEventListener('click', () => {
+    location.href = "holiday-shopping/index.html";
+})
+
+let faq = document.querySelector('[data-faq]');
+faq.addEventListener('click', () => {
+    location.href = "dropdown-faq-example/index.html";
+})
