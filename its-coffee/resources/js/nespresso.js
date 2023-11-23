708ecaeab1913a63469db4a83bece51485405dfd
monkeydluthy <140854666+monkeydluthy@users.noreply.github.com>
2023-11-23 12:27:01 -0500

    Coffee site updates

---
+++ its-coffee/resources/js/nespresso.js
@@ -0,0 +1,89 @@
+let vertuoLineCombo = document.getElementById('vertuo');
+vertuoLineCombo.addEventListener('click', () => {
+    location.href = "https://amzn.to/40E90yS"; 
+});
+
+let melozio = document.getElementById('melozio');
+melozio.addEventListener('click', () => {
+    location.href = "https://amzn.to/49DgVQY";
+});
+
+let barista = document.getElementById('barista');
+barista.addEventListener('click', () => {
+    location.href = "https://amzn.to/3T0Gshl";
+});
+
+let doubleEspresso = document.getElementById('double-espresso');
+doubleEspresso.addEventListener('click', () => {
+    location.href = "https://amzn.to/3uqXuKU";
+});
+
+let ispirazione = document.getElementById('ispirazione');
+ispirazione.addEventListener('click', () => {
+    location.href = "https://amzn.to/3MOves2";
+});
+
+let solelio = document.getElementById('solelio');
+solelio.addEventListener('click', () => {
+    location.href = "https://amzn.to/46jQPQ0";
+});
+
+let lungo = document.getElementById('lungo');
+lungo.addEventListener('click', () => {
+    location.href = "https://amzn.to/3MM7dSA";
+});
+
+let halfcaf = document.getElementById('halfcaf');
+halfcaf.addEventListener('click', () => {
+    location.href = "https://amzn.to/3QJOMin";
+});
+
+let darkstar = document.getElementById('starbucks-dark');
+darkstar.addEventListener('click', () => {
+    location.href = "https://amzn.to/47yje63";
+});
+
+let medstar = document.getElementById('starbucks-med');
+medstar.addEventListener('click', () => {
+    location.href = "https://amzn.to/3FZZoF8";
+});
+
+let intensestar = document.getElementById('starbucks-intense');
+intensestar.addEventListener('click', () => {
+    location.href = "https://amzn.to/47B98Bh";
+});
+
+let mildvar = document.getElementById('mild-var');
+mildvar.addEventListener('click', () => {
+    location.href = "https://amzn.to/47dNVxy";
+});
+
+let machine1 = document.getElementById('machine-1');
+machine1.addEventListener('click', () => {
+    location.href = "https://amzn.to/47BI9Wh";
+});
+
+let breville = document.getElementById('breville');
+breville.addEventListener('click', () => {
+    location.href = "https://amzn.to/49DG91G";
+});
+
+let inissia = document.getElementById('inissia');
+inissia.addEventListener('click', () => {
+    location.href = "https://amzn.to/3MNMaii";
+});
+
+let ninja = document.getElementById('ninja');
+ninja.addEventListener('click', () => {
+    location.href = "https://amzn.to/3MLWqYH";
+});
+
+let mini = document.getElementById('mini');
+mini.addEventListener('click', () => {
+    location.href = "https://amzn.to/49FhGZO";
+});
+
+let essenza = document.getElementById('essenza');
+essenza.addEventListener('click', () => {
+    location.href = "https://amzn.to/47khYnw";
+});
